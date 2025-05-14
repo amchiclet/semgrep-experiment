065ffe4f7952cc3631890067091efb89d383ff00
Amarin Phaosawasdi <amarin@semgrep.com>
2025-05-14 11:14:00 -0700

    add src directory to test excludes

---
+++ src/C.java
@@ -0,0 +1,14 @@
+class C {
+    int f() {
+        return input()
+    }
+    int g() {
+        int y = f();
+    }
+    int h() {
+        int y = input();
+    }
+    int i() {
+        int y = 0;
+    }
+}
